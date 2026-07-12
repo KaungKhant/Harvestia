e4f315cadaa8a24503ee3d1348a242e5e5ce6f17
Nyan-pixel <nyanlhtike.01@gmail.com>
2026-07-12 15:26:28 +0630

    n

--- scene/characters/guide/guide.gd
+++ scene/characters/guide/guide.gd
@@ -8,44 +8,44 @@ var balloon_scene = preload("res://Dialog/game_dialogue_balloon.tscn")
 var in_range: bool
 
 func _ready() -> void:
-	interactable_component.interactable_activated.connect(on_interactable_activated)
-	interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
-	interactable_label_component.hide()
-	
-	GameDialogueManager.teach_farming.connect(on_teach_farming)
-	
+    interactable_component.interactable_activated.connect(on_interactable_activated)
+    interactable_component.interactable_deactivated.connect(on_interactable_deactivated)
+    interactable_label_component.hide()
+    
+    GameDialogueManager.teach_farming.connect(on_teach_farming)
+    
 func on_interactable_activated() -> void:
-	interactable_label_component.show()
-	in_range = true
+    interactable_label_component.show()
+    in_range = true
 
 func on_interactable_deactivated() -> void:
-	interactable_label_component.hide()
-	in_range = false
+    interactable_label_component.hide()
+    in_range = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if !in_range:
-		return
+    if !in_range:
+        return
 
-	if !event.is_action_pressed("show_dialogue"):
-		return
+    if !event.is_action_pressed("show_dialogue"):
+        return
 
-	var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
-	get_tree().current_scene.add_child(balloon)
+    var balloon: BaseGameDialogueBalloon = balloon_scene.instantiate()
+    get_tree().current_scene.add_child(balloon)
 
-	var dialogue: DialogueResource
+    var dialogue: DialogueResource
 
-	if QuestManager.current_quest == null:
-		dialogue = load("res://Dialog/Guide/intro.dialogue")
-	else:
-		dialogue = QuestManager.current_quest.dialogue_file
+    if QuestManager.current_quest == null:
+        dialogue = load("res://Dialog/Guide/intro.dialogue")
+    else:
+        dialogue = QuestManager.current_quest.dialogue_file
 
-	balloon.start(
-		dialogue,
-		QuestManager.get_dialogue_label()
+    balloon.start(
+        dialogue,
+        QuestManager.get_dialogue_label()
 )
 
 func on_teach_farming() -> void:
-	# Farming basics only
-	ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
-	ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
-	ToolManager.enable_tool_button(DataTypes.Tools.PlantCorn)
+    # Farming basics only
+    ToolManager.enable_tool_button(DataTypes.Tools.TillGround)
+    ToolManager.enable_tool_button(DataTypes.Tools.WaterCrops)
+    ToolManager.enable_tool_button(DataTypes.Tools.PlantCorn)
--- scene/components/test_scene_save_data_manager_component.gd
+++ scene/components/test_scene_save_data_manager_component.gd
@@ -3,7 +3,7 @@ extends Node
 
 
 func _ready() -> void:
-	call_deferred("load_test_scene")
-	
+    call_deferred("load_test_scene")
+    
 func load_test_scene():
-	SaveGameManager.load_game()
+    SaveGameManager.load_game()
