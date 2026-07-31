3a37ff00032c71ba4ae4a4f024533a643f80d922
Nyan-pixel <nyanlhtike.01@gmail.com>
2026-08-01 03:05:55 +0630

    Revert "h"
    This reverts commit 4a237959fe9ce851c0486f1fb2d1e70a0bdbb607.

--- scene/objects/house/interior.gd
+++ scene/objects/house/interior.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 var entered = false
 
-func _on_exit_body_entered(body: Node2D) -> void:
-    entered = true
-
+func _on_exit_body_entered(body):
+    if body.is_in_group("player"):
+        get_tree().change_scene_to_file("res://scene/test/test_tilemap_update_tilled_land.tscn")
 func _on_exit_body_exited(body: Node2D) -> void:
     if entered:
-        get_tree().change_scene_to_file("res://scene/test/test_tilemap_chest_test.tscn")
+        get_tree().change_scene_to_file("res://scene/test/test_tilemap_update_tilled_land.tscn")
