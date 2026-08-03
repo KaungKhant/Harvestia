240a2dd408de01e704ba139064b1290649e7bd70
KaungKhant <kaungkhantredh7@gmail.com>
2026-08-04 00:42:38 +0630

    fixing carrot and pumpkin

--- scene/objects/plants/corn_harvest.gd
+++ scene/objects/plants/corn_harvest.gd
@@ -3,17 +3,17 @@ class_name CornHarvest
 
 @export var item_name: String = "corn"
 func _ready():
-    add_to_group("harvest_items")
+	add_to_group("harvest_items")
 func get_item_name() -> String:
-    return item_name
+	return item_name
 
 
 func set_item_name(value: String) -> void:
-    item_name = value
+	item_name = value
 
 
 func _on_body_entered(body: Node2D) -> void:
-    if body is Player:
-        InventoryManager.add_collectable(item_name)
-        QuestManager.add_progress(item_name)
-        queue_free()
+	if body is Player:
+		InventoryManager.add_collectable(item_name)
+		QuestManager.add_progress(item_name)
+		queue_free()
