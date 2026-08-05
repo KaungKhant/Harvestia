70b6e96bda3a007812051e1566b7d315fc2f7947
Brain-0009 <shwehtunlu093@gmail.com>
2026-08-06 00:02:29 +0630

    aa

--- scene/components/collectable_component.gd
+++ scene/components/collectable_component.gd
@@ -11,6 +11,7 @@ extends Area2D
 		#get_parent().queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
+<<<<<<< HEAD
 	if body is Player:
 		InventoryManager.add_collectable(collectable_name)
 		
@@ -24,3 +25,20 @@ func _on_body_entered(body: Node2D) -> void:
 
 		print("Collected:", collectable_name)
 		get_parent().queue_free()
+=======
+    if body is Player:
+
+        InventoryManager.add_collectable(collectable_name)
+        QuestManager.add_progress(collectable_name)
+        PlayerProgressManager.award_crop_exp(collectable_name)
+
+        var harvest = get_parent()
+
+        harvest.remove_from_group("save_data_component")
+
+        var save_component = harvest.get_node_or_null("SaveDataComponent")
+        if save_component:
+            save_component.queue_free()
+
+        harvest.queue_free()
+>>>>>>> 14ffdd6bb963e1bf30349ef10a93dae7394d2eaf
