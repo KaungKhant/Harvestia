f54a61c275a49293368054dbd58dd1aa8ce1fded
Brain-0009 <shwehtunlu093@gmail.com>
2026-08-06 00:14:23 +0630

    update

--- scene/components/collectable_component.gd
+++ scene/components/collectable_component.gd
@@ -11,34 +11,18 @@ extends Area2D
 		#get_parent().queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
-<<<<<<< HEAD
 	if body is Player:
+
 		InventoryManager.add_collectable(collectable_name)
-		
-		print(InventoryManager.inventory)
-		
 		QuestManager.add_progress(collectable_name)
-
 		PlayerProgressManager.award_crop_exp(collectable_name)
-		
-		var exp = PlayerProgressManager.crop_exp.get(collectable_name, 0)
 
-		print("Collected:", collectable_name)
-		get_parent().queue_free()
-=======
-    if body is Player:
+		var harvest = get_parent()
 
-        InventoryManager.add_collectable(collectable_name)
-        QuestManager.add_progress(collectable_name)
-        PlayerProgressManager.award_crop_exp(collectable_name)
+		harvest.remove_from_group("save_data_component")
 
-        var harvest = get_parent()
+		var save_component = harvest.get_node_or_null("SaveDataComponent")
+		if save_component:
+			save_component.queue_free()
 
-        harvest.remove_from_group("save_data_component")
-
-        var save_component = harvest.get_node_or_null("SaveDataComponent")
-        if save_component:
-            save_component.queue_free()
-
-        harvest.queue_free()
->>>>>>> 14ffdd6bb963e1bf30349ef10a93dae7394d2eaf
+		harvest.queue_free()
