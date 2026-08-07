1b41019f3ecd3473eb9ed028c9c1647ab8019def
KaungKhant <kaungkhantredh7@gmail.com>
2026-08-07 17:20:15 +0630

    carrot error fix

--- scene/components/crop_cursor_component.gd
+++ scene/components/crop_cursor_component.gd
@@ -19,120 +19,120 @@ var distance: float
 
 
 func _unhandled_input(event: InputEvent) -> void:
-    if event.is_action_pressed("remove_dirt"):
-        if ToolManager.selected_tool == DataTypes.Tools.TillGround:
-            get_cell_under_mouse()
-            remove_crop()
+	if event.is_action_pressed("remove_dirt"):
+		if ToolManager.selected_tool == DataTypes.Tools.TillGround:
+			get_cell_under_mouse()
+			remove_crop()
 
-    elif event.is_action_pressed("hit"):
-        if ToolManager.selected_tool == DataTypes.Tools.PlantCorn \
-        or ToolManager.selected_tool == DataTypes.Tools.PlantTomato \
-        or ToolManager.selected_tool == DataTypes.Tools.PlantCarrot \
-        or ToolManager.selected_tool == DataTypes.Tools.PlantPumpkin:
-            get_cell_under_mouse()
-            add_crop()
+	elif event.is_action_pressed("hit"):
+		if ToolManager.selected_tool == DataTypes.Tools.PlantCorn \
+		or ToolManager.selected_tool == DataTypes.Tools.PlantTomato \
+		or ToolManager.selected_tool == DataTypes.Tools.PlantCarrot \
+		or ToolManager.selected_tool == DataTypes.Tools.PlantPumpkin:
+			get_cell_under_mouse()
+			add_crop()
 
 
 func get_cell_under_mouse() -> void:
-    mouse_position = tilled_soil_tilemap_layer.get_local_mouse_position()
-    cell_position = tilled_soil_tilemap_layer.local_to_map(mouse_position)
-    cell_source_id = tilled_soil_tilemap_layer.get_cell_source_id(cell_position)
-    local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
-    distance = player.global_position.distance_to(local_cell_position)
+	mouse_position = tilled_soil_tilemap_layer.get_local_mouse_position()
+	cell_position = tilled_soil_tilemap_layer.local_to_map(mouse_position)
+	cell_source_id = tilled_soil_tilemap_layer.get_cell_source_id(cell_position)
+	local_cell_position = tilled_soil_tilemap_layer.map_to_local(cell_position)
+	distance = player.global_position.distance_to(local_cell_position)
 
 
 func add_crop() -> void:
-    if distance >= 20.0:
-        return
+	if distance >= 20.0:
+		return
 
-    if cell_source_id == -1:
-        return
+	if cell_source_id == -1:
+		return
 
-    # Check if a crop already exists on this tile
-    for node in crop_fields.get_children():
+	# Check if a crop already exists on this tile
+	for node in crop_fields.get_children():
 
-        if !is_instance_valid(node):
-            continue
+		if !is_instance_valid(node):
+			continue
 
-        if node.is_queued_for_deletion():
-            continue
+		if node.is_queued_for_deletion():
+			continue
 
-        # Ignore dropped harvest items
-        if node is CornHarvest:
-            continue
+		# Ignore dropped harvest items
+		if node is CornHarvest or node is CarrotHarvest:
+			continue
 
-        var node_cell = tilled_soil_tilemap_layer.local_to_map(node.global_position)
+		var node_cell = tilled_soil_tilemap_layer.local_to_map(node.global_position)
 
-        if node_cell == cell_position:
-            return
-
-
-    match ToolManager.selected_tool:
-
-        DataTypes.Tools.PlantCorn:
-            if InventoryManager.inventory["corn_seeds"] <= 0:
-                return
-
-            var crop = corn_plant_scene.instantiate()
-            crop.global_position = local_cell_position
-            crop_fields.add_child(crop)
-
-            InventoryManager.inventory["corn_seeds"] -= 1
+		if node_cell == cell_position:
+			return
 
 
-        DataTypes.Tools.PlantTomato:
-            if InventoryManager.inventory["tomato_seeds"] <= 0:
-                return
+	match ToolManager.selected_tool:
 
-            var crop = tomato_plant_scene.instantiate()
-            crop.global_position = local_cell_position
-            crop_fields.add_child(crop)
+		DataTypes.Tools.PlantCorn:
+			if InventoryManager.inventory["corn_seeds"] <= 0:
+				return
 
-            InventoryManager.inventory["tomato_seeds"] -= 1
+			var crop = corn_plant_scene.instantiate()
+			crop.global_position = local_cell_position
+			crop_fields.add_child(crop)
 
-
-        DataTypes.Tools.PlantCarrot:
-            if InventoryManager.inventory["carrot_seeds"] <= 0:
-                return
-
-            var crop = carrot_plant_scene.instantiate()
-            crop.global_position = local_cell_position
-            crop_fields.add_child(crop)
-
-            InventoryManager.inventory["carrot_seeds"] -= 1
+			InventoryManager.inventory["corn_seeds"] -= 1
 
 
-        DataTypes.Tools.PlantPumpkin:
-            if InventoryManager.inventory["pumpkin_seeds"] <= 0:
-                return
+		DataTypes.Tools.PlantTomato:
+			if InventoryManager.inventory["tomato_seeds"] <= 0:
+				return
 
-            var crop = pumpkin_plant_scene.instantiate()
-            crop.global_position = local_cell_position
-            crop_fields.add_child(crop)
+			var crop = tomato_plant_scene.instantiate()
+			crop.global_position = local_cell_position
+			crop_fields.add_child(crop)
 
-            InventoryManager.inventory["pumpkin_seeds"] -= 1
+			InventoryManager.inventory["tomato_seeds"] -= 1
 
 
-    InventoryManager.inventory_changed.emit()
+		DataTypes.Tools.PlantCarrot:
+			if InventoryManager.inventory["carrot_seeds"] <= 0:
+				return
+
+			var crop = carrot_plant_scene.instantiate()
+			crop.global_position = local_cell_position
+			crop_fields.add_child(crop)
+
+			InventoryManager.inventory["carrot_seeds"] -= 1
+
+
+		DataTypes.Tools.PlantPumpkin:
+			if InventoryManager.inventory["pumpkin_seeds"] <= 0:
+				return
+
+			var crop = pumpkin_plant_scene.instantiate()
+			crop.global_position = local_cell_position
+			crop_fields.add_child(crop)
+
+			InventoryManager.inventory["pumpkin_seeds"] -= 1
+
+
+	InventoryManager.inventory_changed.emit()
 
 
 func remove_crop() -> void:
-    if distance >= 25.0:
-        return
+	if distance >= 25.0:
+		return
 
-    for node in crop_fields.get_children():
+	for node in crop_fields.get_children():
 
-        if !is_instance_valid(node):
-            continue
+		if !is_instance_valid(node):
+			continue
 
-        if node.is_queued_for_deletion():
-            continue
+		if node.is_queued_for_deletion():
+			continue
 
-        if node is CornHarvest:
-            continue
+		if node is CornHarvest:
+			continue
 
-        var node_cell = tilled_soil_tilemap_layer.local_to_map(node.global_position)
+		var node_cell = tilled_soil_tilemap_layer.local_to_map(node.global_position)
 
-        if node_cell == cell_position:
-            node.queue_free()
-            return
+		if node_cell == cell_position:
+			node.queue_free()
+			return
--- scene/objects/plants/carrot_2.gd
+++ scene/objects/plants/carrot_2.gd
@@ -3,7 +3,7 @@ extends Node2D
 # 🟢 FIX: Removed the duplicate '@export var scene_file_path' definition!
 # Godot already provides 'scene_file_path' automatically for all nodes.
 
-var pumpkin_harvest_scene = preload("res://scene/objects/plants/pumpkin_harvest.tscn")
+var corn_harvest_scene = preload("res://scene/objects/plants/corn_harvest.tscn")
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var watering_particles: GPUParticles2D = $WateringParticles
@@ -70,7 +70,7 @@ func on_crop_maturity() -> void:
 func on_crop_harvesting() -> void:
 	growth_cycle_components.harvested = true
 
-	var harvest = pumpkin_harvest_scene.instantiate()
+	var harvest = corn_harvest_scene.instantiate()
 	harvest.global_position = global_position
 	get_parent().add_child(harvest)
 
