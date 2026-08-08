a0ff43032d7dd1f8c59f7266e301685ddbfacba1
Nyan-pixel <nyanlhtike.01@gmail.com>
2026-08-08 11:31:09 +0630

    intro upgrade

--- scripts/globals/game_dialogue_manager.gd
+++ scripts/globals/game_dialogue_manager.gd
@@ -11,32 +11,32 @@ signal open_homecoming_board
 signal start_homecoming_ceremony
 
 func action_teach_farming() -> void:
-	teach_farming.emit()
+    teach_farming.emit()
 
 func action_feed_animals() -> void:
-	feed_the_animals.emit()
+    feed_the_animals.emit()
 
 func action_unlock_forest() -> void:
-	unlock_forest.emit()
+    unlock_forest.emit()
 
 func action_give_axe() -> void:
-	give_axe.emit()
+    give_axe.emit()
 
 func action_give_pickaxe() -> void:
-	give_pickaxe.emit()
+    give_pickaxe.emit()
 
 func action_transfer_chicken_coop() -> void:
-	transfer_chicken_coop.emit()
+    transfer_chicken_coop.emit()
 
 func action_transfer_cow_barn() -> void:
-	transfer_cow_barn.emit()
-	
+    transfer_cow_barn.emit()
+    
 func action_finish_grandfather_story() -> void:
-	PlayerProgressManager.talked_to_village_elder = true
+    PlayerProgressManager.talked_to_village_elder = true
 
 func action_open_homecoming_board() -> void:
-	open_homecoming_board.emit()
+    open_homecoming_board.emit()
 
 
 func action_start_homecoming_ceremony() -> void:
-	start_homecoming_ceremony.emit()
+    start_homecoming_ceremony.emit()
